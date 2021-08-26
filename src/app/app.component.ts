9655b1d0d5c6957493db6bfea81bcd29ab4cf720
nicolaspantano <nicolaspantano757@gmail.com>
2021-08-26 20:24:47 -0300

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'clase1';
+  edadUno='';
+  edadDos='';
+  promedio:any;
+
+  
+  limpiar(){
+    this.edadUno='';
+    this.edadDos='';
+    this.promedio='';
+  }
+
+  calcular(){
+    this.promedio = (parseInt(this.edadUno) + parseInt(this.edadDos))/2;
+  }
 }
